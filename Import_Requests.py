31c3c3f074a1c8d84337b8d6b7612c7d7ed5217e
Abhishek <abhishek.dwivediaug11@gmail.com>
2024-11-19 19:09:09 +0530

    Making some feature changes.

--- Import_Requests.py
+++ Import_Requests.py
@@ -2,6 +2,5 @@ url = 'https://screener.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Extract data from the webpage
 data = soup.find('div', class_='content')
 print(data.text)
