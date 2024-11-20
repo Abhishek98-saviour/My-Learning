3f2d442bf81b09ddfd25ea052fad5d61a871fe0d
Abhishek <abhishek.dwivediaug11@gmail.com>
2024-11-20 17:38:15 +0530

    making changes to import request

--- Import_Requests.py
+++ Import_Requests.py
@@ -1,4 +1,7 @@
-url = 'https://screener.com'
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://example.com'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
